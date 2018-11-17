289d30f2f4c32fc8bddddd91f330dd080e5c3551
FutureScript <fernandobaxter1@gmail.com>
2018-11-17 21:48:56 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,24 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  i= 0
+  new_array = []
+  while i < array.length
+  new_array << yield array[i]
+  i +=1
+end 
+new_array 
 end
+
+
+# def my_each(array)
+#   while array< 10
+#     yield "hello there"
+# end
+# end
+# my_each
+# end
+
+
+
+
+  # put argument(s) here
+  # code here
